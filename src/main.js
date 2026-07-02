2fa73a86468700927deee634f704d88f290bffd7
simka <semirenkonasta81@gmail.com>
2026-07-02 15:19:35 +0300

    base setting

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './js/header';
+import './js/hero';
+import './js/about';
+import './js/benefits';
+import './js/feedbacks';
+import './js/portfolio';
+import './js/faq';
+import './js/contacts';
+import './js/footer';
+import './js/success-modal';
