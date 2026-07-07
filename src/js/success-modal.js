262f6d555528d6957a1bb7d772c02934977acb28
Anna Tomchenko <anytom6996@gmail.com>
2026-07-07 10:42:47 +0300

    fix: use MutationObserver to sync body scroll lock regardless of how is-hidden class is toggled

--- src/js/success-modal.js
+++ src/js/success-modal.js
@@ -4,14 +4,10 @@
 
   function openSuccessModal() {
     overlay.classList.remove("is-hidden");
-    document.body.classList.add("modal-open");
-    document.addEventListener("keydown", handleEscapeKey);
   }
 
   function closeSuccessModal() {
     overlay.classList.add("is-hidden");
-    document.body.classList.remove("modal-open");
-    document.removeEventListener("keydown", handleEscapeKey);
   }
 
   function handleEscapeKey(e) {
@@ -19,6 +15,20 @@
       closeSuccessModal();
     }
   }
+
+  // Слідкуємо за зміною класу is-hidden, незалежно від того, хто його змінив
+  const observer = new MutationObserver(() => {
+    const isVisible = !overlay.classList.contains("is-hidden");
+    if (isVisible) {
+      document.body.classList.add("modal-open");
+      document.addEventListener("keydown", handleEscapeKey);
+    } else {
+      document.body.classList.remove("modal-open");
+      document.removeEventListener("keydown", handleEscapeKey);
+    }
+  });
+
+  observer.observe(overlay, { attributes: true, attributeFilter: ["class"] });
 
   closeBtn.addEventListener("click", closeSuccessModal);
 
