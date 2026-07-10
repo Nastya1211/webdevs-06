e18319d3be1203387ab87754c5da7f4d95fd9338
simka <semirenkonasta81@gmail.com>
2026-07-10 20:39:01 +0300

    final fix

--- src/main.js
+++ src/main.js
@@ -3,12 +3,8 @@ import 'izitoast/dist/css/iziToast.min.css';
 import './js/loader';
 
 import './js/header';
-import './js/hero';
-import './js/about';
-import './js/benefits';
 import './js/feedbacks';
 import './js/portfolio';
 import './js/faq';
 import './js/contacts';
-import './js/footer';
 import './js/success-modal';
